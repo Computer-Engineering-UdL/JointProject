70e18b0ec396134c7d7edf2c7b99d6e81399e944
Aniol0012 <aniol0012@gmail.com>
2024-02-18 12:22:59 +0100

    init: fixed run issue

--- JointProject/urls.py
+++ JointProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for src project.
+URL configuration for JointProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
