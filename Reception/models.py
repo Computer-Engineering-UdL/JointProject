dc7779911baa930514a2bb219a41867a25516dad
Abdellah Lamrabat <udlunieps@gmail.com>
2024-03-21 10:25:38 +0100

    feat: Can modify the database in the admin page

--- Reception/models.py
+++ Reception/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import Model
-
 
 # Create your models here.
 class Worker(models.Model):
@@ -11,11 +9,15 @@ class Worker(models.Model):
     def __str__(self):
         return self.user.first_name
 
+
 class Client(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_hosted = models.BooleanField()
+
     def __str__(self):
         return self.user.first_name
+
 
 class Room(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
@@ -23,6 +25,7 @@ class Room(models.Model):
     is_taken = models.BooleanField()
     room_num = models.IntegerField()
     room_price = models.IntegerField()
+
 
     def __str__(self):
         return self.room_num
@@ -34,11 +37,8 @@ class Room_reservation(models.Model):
     pension_type = models.CharField(max_length=100)
     num_guests = models.IntegerField()
 
+
     class Meta:
         unique_together = ('room', 'check_in', 'check_out')
     def __str__(self):
         return self.room.room_num
-
-
-
-
