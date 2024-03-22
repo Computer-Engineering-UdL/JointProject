084f6e33d59d8ad71078df52a77706e267ac1df8
Sergi Fernandez <s.nandes@hotmail.com>
2024-03-22 17:43:16 +0100

    fix: moved views functions from user views to reception views

--- user/views.py
+++ user/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from Reception.forms import AddClientForm, RoomReservationForm
 from user.forms import SignUpForm
 
 
@@ -16,25 +15,3 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
-
-
-def add_client(request):
-    """Add a new client to the database."""
-    if request.method == 'POST':
-        form = AddClientForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = AddClientForm()
-    return render(request, 'add_client.html', {'form': form})
-
-
-def room_reservation(request):
-    """Reserve a room for a client."""
-    if request.method == 'POST':
-        form = RoomReservationForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = RoomReservationForm()
-    return render(request, 'room_reservation.html', {'form': form})
