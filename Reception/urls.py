92b96586d10972156d6cf73837ccb16ea3b7d2e2
Sergi <s.nandes@hotmail.com>
2024-03-22 18:12:26 +0100

    style: flake fixes

--- Reception/urls.py
+++ Reception/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-from Reception.views import add_room,add_client, room_reservation
+from Reception.views import add_room, add_client, room_reservation
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="reception/reception_home.html"), name="home"),
