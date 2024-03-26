d4d2f69da1685142fa24574656ab961fbfa2d14f
Aniol0012 <aniol0012@gmail.com>
2024-03-26 18:59:09 +0100

    refactor: removed default required type

--- user/forms.py
+++ user/forms.py
@@ -6,11 +6,11 @@ UserModel = get_user_model()
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=80, required=True,
+    first_name = forms.CharField(max_length=80,
                                  error_messages={'required': 'Required. Inform your first name.'})
-    last_name = forms.CharField(max_length=80, required=True,
+    last_name = forms.CharField(max_length=80,
                                 error_messages={'required': 'Required. Inform your last name.'})
-    phone_number = forms.CharField(max_length=9, required=True, label='Phone Number',
+    phone_number = forms.CharField(max_length=9, label='Phone Number',
                                    error_messages={'required': 'Required. Select your ID type.'})
 
     ID_TYPE_CHOICES = [
@@ -29,7 +29,7 @@ class SignUpForm(UserCreationForm):
                                 label='ID Number',
                                 error_messages={'required': 'Required. Inform your ID number.'})
 
-    email = forms.EmailField(max_length=100, required=True,
+    email = forms.EmailField(max_length=100,
                              error_messages={'required': 'Required. Inform a valid email address.',
                                              'invalid': 'Invalid email address.'})
 
