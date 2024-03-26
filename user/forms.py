37c6faeed0b9e3921c794aa7bf5e69ff6468d9b7
Aniol0012 <aniol0012@gmail.com>
2024-03-26 15:02:10 +0100

    feat: add signup form fields

--- user/forms.py
+++ user/forms.py
@@ -6,6 +6,10 @@ UserModel = get_user_model()
 
 
 class SignUpForm(UserCreationForm):
+    FirstName = forms.CharField(max_length=80, help_text='Required. Inform your first name.')
+    LastName = forms.CharField(max_length=80, help_text='Required. Inform your last name.')
+    PhoneNumber = forms.CharField(max_length=12, label='Phone Number', help_text='Required. Inform your phone number.')
+    IDNumber = forms.CharField(max_length=20, label='DNI, NIE or Passport', help_text='Required. Inform your ID number.')
     email = forms.EmailField(max_length=100, help_text='Required. Inform a valid email address.')
 
     class Meta:
