c490bdc073a7451dc25727b3274d8dba6411030c
Aniol <aniol0012@gmail.com>
2024-03-31 16:08:17 +0200

    sytle: linting issues

--- JointProject/urls.py
+++ JointProject/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('logout/', User.views.user_logout, name='logout'),
     path('receptionist/', include('Reception.urls')),
     path("", TemplateView.as_view(template_name="base.html"), name="base"),
-    # path("", TemplateView.as_view(template_name="worker/receptionist/receptionist_home.html"), name="recepcionist_home")
+    # path("", TemplateView.as_view(template_name="worker/receptionist/receptionist_home.html"),
+    # name="recepcionist_home")
 ]
