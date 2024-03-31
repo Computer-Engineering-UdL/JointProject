15bffd7865b8c0f5d583ac445bbafcd2e9cf260f
Aniol <aniol0012@gmail.com>
2024-03-31 10:04:04 +0200

    fix: updated checkin view url

--- Reception/tests.py
+++ Reception/tests.py
@@ -13,21 +13,21 @@ class CheckInViewTest(TestCase):
                                                 email='client@gmail.com', phone_number='123456789', is_hosted=False)
 
     def test_check_in_view_status_code(self):
-        url = reverse('check-in')
+        url = reverse('check_in')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_check_in_view_form_errors(self):
-        url = reverse('check-in')
+        url = reverse('check_in')
         response = self.client.post(url, {})
         self.assertContains(response, "Introdueix el número de reserva o el número del document identificatiu")
 
     def test_check_in_view_redirect_with_reservation(self):
-        url = reverse('check-in')
+        url = reverse('check_in')
         response = self.client.post(url, {'num_reservation': self.reservation.id})
         self.assertTemplateUsed(response, 'worker/receptionist/check-in/check_in_2.html')
 
     def test_check_in_view_redirect_with_dni(self):
-        url = reverse('check-in')
+        url = reverse('check_in')
         response = self.client.post(url, {'dni': self.client_obj.id_number})
         self.assertTemplateUsed(response, 'worker/receptionist/check-in/check_in_2.html')
