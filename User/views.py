bf3def069b27027b36a82157c4cc6262dd9f0ddd
Aniol0012 <aniol0012@gmail.com>
2024-04-02 23:43:50 +0200

    feat: login and signup redirects depending on worker type

--- User/views.py
+++ User/views.py
@@ -6,7 +6,20 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from JointProject.settings import LOGOUT_REDIRECT_URL
 from User.decorators import worker_required
-from Reception.models import Worker
+
+
+def redirect_user_based_on_type(user):
+    """Determines the URL depending on the type of User."""
+    # Workers
+    if hasattr(user, 'worker'):
+        worker_type_to_url = {
+            'worker': 'worker_home',
+            'receptionist': 'receptionist_home',
+        }
+        return worker_type_to_url.get(user.worker.type, 'base')
+    # Not workers
+    else:
+        return 'base'
 
 
 def signup(request):
@@ -16,10 +29,10 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)  # Log in the User after signing up
-            messages.success(request, 'Account created successfully!')
-            return redirect('base')
+            messages.success(request, 'Account created successfully')
+            return redirect(redirect_user_based_on_type(user))
         else:
-            messages.error(request, 'Please correct the error below.')
+            messages.error(request, 'Please correct the error below')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
@@ -34,17 +47,11 @@ def login_custom(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                if hasattr(user, 'worker'):
-                    if user.worker.type == 'receptionist':
-                        return redirect('receptionist_home')
-                    else:
-                        return redirect('worker_home')
-                else:
-                    return redirect('base')
+                return redirect(redirect_user_based_on_type(user))
             else:
-                messages.error(request, "Invalid username or password.")
+                messages.error(request, "Invalid username or password")
         else:
-            messages.error(request, "Invalid username or password.")
+            messages.error(request, "Invalid username or password")
     else:
         form = AuthenticationForm()
     return render(request, 'registration/login.html', {'form': form})
