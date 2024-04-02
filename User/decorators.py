b82e8bd286390e860982e5369287b0321223b7b6
Aniol0012 <aniol0012@gmail.com>
2024-04-02 17:05:54 +0200

    docs: add decorators documentation

--- User/decorators.py
+++ User/decorators.py
@@ -7,6 +7,7 @@ UserModel = get_user_model()
 
 
 def worker_required(worker_type=None):
+    """Allows only the worker specified to access the view. Admins can access all views."""
     def decorator(view_func):
         @wraps(view_func)
         @login_required
@@ -27,6 +28,7 @@ def worker_required(worker_type=None):
 
 
 def admin_required(view_func):
+    """Allows only the admin to access the view."""
     @wraps(view_func)
     @login_required
     def _wrapped_view(request, *args, **kwargs):
