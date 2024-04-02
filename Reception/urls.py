2e7ea2292c1e71d8e4d6747b9f0a0b43bbc48ca9
Abdellah Lamrabat <udlunieps@gmail.com>
2024-04-02 13:19:20 +0200

    feat: all steps of check-in are implemented and all tests have passed

--- Reception/urls.py
+++ Reception/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from Reception.views import (worker_home, add_client, room_reservation, add_room, check_in_1, fetch_rooms,
-                             search_reservation, reservation_details, delete_reservation)
+                             search_reservation, reservation_details, delete_reservation, check_in_summary,
+                             print_receipt)
 
 
 urlpatterns = [
@@ -12,6 +13,8 @@ urlpatterns = [
     path('room_reservation/', room_reservation, name='room_reservation'),
     path('add_room/', add_room, name='add_room'),
     path('check-in/', check_in_1, name='check_in'),
+    path('check-in/summary/', check_in_summary, name='check_in_summary'),
+    path('print_receipt/<int:client_id>/<int:reservation_id>/', print_receipt, name='print_receipt'),
     path('fetch_rooms/', fetch_rooms, name='fetch_rooms'),
     path('search_reservation/', search_reservation, name='search_reservation'),
     path('reservation_details/<int:pk>/', reservation_details, name='reservation_details'),
