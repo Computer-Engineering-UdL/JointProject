9a216a0dae63944c6d77919d07f0c0c78c2126f4
Sergi Fernandez Espona <s.nandes@hotmail.com>
2024-04-03 11:45:20 +0200

    feat: changed dict structure

--- Reception/config.py
+++ Reception/config.py
@@ -3,21 +3,10 @@ class Config:
     def get_room_extra_costs():
         return Config.room_extra_costs
 
-    @staticmethod
-    def get_room_type_costs():
-        return Config.room_type_costs
-
-    room_extra_costs = {
-        'Minibar': [10],
-        'Desperfectes': [20],
-        'Servei habitacions': [15],
-        'Pàrquing': [5],
-        'Pèrdua de claus': [20]
-    }
-
-    room_type_costs = {
-        'Individual': [50],
-        'Double': [75],
-        'Suite': [100],
-        'Deluxe': [125]
-    }
+    room_extra_costs = [
+        ('Minibar', 'Minibar'),
+        ('Desperfectes', 'Desperfectes'),
+        ('Servei habitacions', 'Servei habitacions'),
+        ('Pàrking', 'Parking'),
+        ('Pèrdua de claus', 'Pèrdua de claus'),
+    ]
