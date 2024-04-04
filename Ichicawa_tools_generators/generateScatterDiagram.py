ae226b45dfba1d044bf11260293890f94f1e5ae6
Aniol0012 <aniol0012@gmail.com>
2024-04-04 18:34:01 +0200

    feat: update ichicawa tools

--- Ichicawa_tools_generators/generateScatterDiagram.py
+++ Ichicawa_tools_generators/generateScatterDiagram.py
@@ -1,31 +1,21 @@
 import os
-from github import Github
 import matplotlib.pyplot as plt
+from github import Github
+from generate_histogram import repo_name
 
 # This code generates a scatter diagram with all the
-# commits of the repository, showing the added lines and
-# the modified files. It has to be executed manually
+# issues of the repository, showing the creation data and
+# the days for closing it. It has to be executed manually
 # using the "scatterGeneralReportGenerator.yml" workflow
 
-
 token = os.getenv('GITHUB_TOKEN')
-repo_name = "Computer-Engineering-UdL/JointProject"
+
 
 g = Github(token)
 repo = g.get_repo(repo_name)
 
-lines_added_per_commit = []
-files_modified_per_commit = []
-authors_of_commits = []
-
-# Modify author names and colors for your repository
-author_colors = {
-    "SergiFn": "blue",
-    "Aniol0012": "red",
-    "AbdeDevs": "green",
-    "julianogales": "orange",
-    "Abelitux": "purple",
-}
+days_to_close = []
+issue_created_dates = []
 
 
 def main() -> None:
@@ -34,41 +24,30 @@ def main() -> None:
 
 
 def get_data() -> None:
-    """Gets all the lines added and files modified per commit"""
-    commits = repo.get_commits()
+    """Gets all the closed issues with their open date and time to close"""
+    issues = repo.get_issues(state='closed')
+    for issue in issues:
+        created_at = issue.created_at
+        closed_at = issue.closed_at
 
-    for commit in commits:
-        lines_added = commit.stats.additions
-        files_modified = len(commit.files)
-        author_name = commit.author.login
-
-        lines_added_per_commit.append(lines_added)
-        files_modified_per_commit.append(files_modified)
-        authors_of_commits.append(author_name)
+        if closed_at is not None:
+            time_to_close = (closed_at - created_at).days
+            days_to_close.append(time_to_close)
+            issue_created_dates.append(created_at)
 
 
 def generate_diagram() -> None:
     """Generates and saves the scatter diagram with all the data"""
     plt.figure(figsize=(10, 6))
-    legend_authors = set()
-    for author, lines_added, files_modified in zip(authors_of_commits, lines_added_per_commit,
-                                                   files_modified_per_commit):
-        color = author_colors.get(author, "black")  # Default to black if author not in author_colors
 
-        if author not in legend_authors:
+    plt.scatter(days_to_close, issue_created_dates, color='blue', alpha=0.7)
 
-            plt.scatter(lines_added, files_modified, color=color, label=author)
-            legend_authors.add(author)
-        else:
-            plt.scatter(lines_added, files_modified, color=color)
-
-    plt.xlabel('Lines added')
-    plt.ylabel('Files modified')
-    plt.title('Scatter Diagram of Lines added and Files modified')
-    plt.legend()
+    plt.xlabel('Days for closing the issue')
+    plt.ylabel('Date of creation of the issue')
+    plt.title('Scatter Diagram of closed issues')
     plt.grid(True)
 
-    plt.savefig("scatter_diagram_commits.png")
+    plt.savefig("scatter_diagram_issues.png")
 
 
 if __name__ == '__main__':
--- Ichicawa_tools_generators/generateWeeklyScatterDiagram.py
+++ Ichicawa_tools_generators/generateWeeklyScatterDiagram.py
@@ -1,21 +1,20 @@
-import os
 from datetime import datetime, timezone, timedelta
-from github import Github
-import matplotlib.pyplot as plt
-from generateScatterDiagram import token, repo_name, author_colors
 
-# This code generates a scatter diagram with all the commits
-# done on this week, showing the added lines and the modified
-# files. It runs automatically every Sunday on 23:59 using the
+import matplotlib.pyplot as plt
+from github import Github
+
+from generateScatterDiagram import token, repo_name
+
+# This code generates a scatter diagram with all the issues
+# closed on this week, showing the creation data and the days for
+# closing. It runs automatically every Sunday on 23:59 using the
 # "scatterWeeklyGenerator.yml" workflow.
 
 g = Github(token)
 repo = g.get_repo(repo_name)
 
-lines_added_per_commit = []
-files_modified_per_commit = []
-
-authors_of_commits = []
+days_to_close = []
+issue_created_dates = []
 
 
 def main() -> None:
@@ -34,41 +33,32 @@ def get_date_range_of_current_week() -> tuple:
 
 
 def get_data() -> None:
-    """Gets all the lines added and files modified per commit"""
+    """Gets all the closed issues with their open date and time to close"""
     start_of_week, end_of_week = get_date_range_of_current_week()
-    commits = repo.get_commits(since=start_of_week)
+    issues = repo.get_issues(state='closed')
 
-    for commit in commits:
-        lines_added = commit.stats.additions
-        files_modified = len(commit.files)
-        author_name = commit.author.login
+    for issue in issues:
+        closed_at = issue.closed_at
 
-        lines_added_per_commit.append(lines_added)
-        files_modified_per_commit.append(files_modified)
-        authors_of_commits.append(author_name)
+        if closed_at is not None and start_of_week <= closed_at:
+            created_at = issue.created_at
+            time_to_close = (closed_at - created_at).days
+            days_to_close.append(time_to_close)
+            issue_created_dates.append(created_at)
 
 
 def generate_diagram() -> None:
     """Generates and saves the scatter diagram with all the data"""
     plt.figure(figsize=(10, 6))
-    legend_authors = set()
 
-    for author, lines_added, files_modified in zip(authors_of_commits, lines_added_per_commit,
-                                                   files_modified_per_commit):
-        color = author_colors.get(author, "black")  # Default to black if author not in author_colors
-        if author not in legend_authors:
-            plt.scatter(lines_added, files_modified, color=color, label=author)
-            legend_authors.add(author)
-        else:
-            plt.scatter(lines_added, files_modified, color=color)
+    plt.scatter(days_to_close, issue_created_dates, color='blue', alpha=0.7)
 
-    plt.xlabel('Lines added')
-    plt.ylabel('Files modified')
-    plt.title(f'Scatter Diagram of Lines added and Files modified - {get_date()}')
-    plt.legend()
+    plt.xlabel('Days for closing the issue')
+    plt.ylabel('Date of creation of the issue')
+    plt.title(f'Scatter Diagram of closed issues - {get_date()}')
     plt.grid(True)
 
-    plt.savefig("scatter_diagram_commits_weekly.png")
+    plt.savefig("scatter_diagram_issues_weekly.png")
 
 
 def get_date() -> str:
