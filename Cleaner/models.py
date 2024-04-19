2d935ee9b5327e6483ad0260e9cfa621ecda67e3
Aniol <aniol0012@gmail.com>
2024-04-19 20:03:04 +0200

    feat: add cleaner models

--- Cleaner/models.py
+++ Cleaner/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Cleaning_Material(models.Model):
+    material_name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='cleaning_materials/')
+
+    def __str__(self):
+        return self.material_name
+
+
+class Stock(models.Model):
+    material = models.ForeignKey(Cleaning_Material, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    is_available = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.material.material_name
