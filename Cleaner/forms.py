39d7465771d6747d2b5cf2b805d1b3b03b085a3f
Aniol0012 <aniol0012@gmail.com>
2024-04-20 23:28:54 +0200

    fix: media path

--- Cleaner/forms.py
+++ Cleaner/forms.py
@@ -3,7 +3,7 @@ from Cleaner.models import Stock, Cleaning_Material
 
 
 class StockForm(forms.ModelForm):
-    material = forms.CharField(label="Introdueix el nom del item a cercar")
+    material = forms.ModelChoiceField(queryset=Cleaning_Material.objects.all(), label="Introdueix el nom del item a cercar", required=False)
 
     class Meta:
         model = Stock
--- Cleaner/models.py
+++ Cleaner/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Cleaning_Material(models.Model):
     material_name = models.CharField(max_length=50)
     description = models.TextField()
-    image = models.ImageField(upload_to='Cleaner/cleaning_materials/')
+    image = models.ImageField(upload_to='cleaning_materials/')
 
     def __str__(self):
         return self.material_name
@@ -11,7 +12,6 @@ class Cleaning_Material(models.Model):
 
 class Stock(models.Model):
     material = models.ForeignKey(Cleaning_Material, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
     price = models.FloatField()
     is_available = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
