8d4e701642ba600dae7abd7eb1255245e8472d10
Sergi Fernandez Espona <s.nandes@hotmail.com>
2024-04-21 00:40:13 +0200

    feat: implemented cleaned room form

--- Cleaner/forms.py
+++ Cleaner/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from Cleaner.models import Stock, Cleaning_Material
+from Cleaner.models import Stock, Cleaning_Material, CleanedRoom
 from Reception.models import Room
 
 
@@ -16,4 +16,10 @@ class CleanedRoomForm(forms.Form):
     occupied_rooms = forms.ModelChoiceField(queryset=Room.objects.filter(is_taken=True,
                                                                          roomreservation__check_out_active=False),
                                             empty_label="Select a client")
+    missing_objects = forms.CharField(label="S'han deixat alguna cosa?", required=False)
+    need_towels = forms.IntegerField(label="Falten tovalloles?", required=False)
+    additional_comments = forms.CharField(label="Comentaris addicionals", required=False)
 
+    class Meta:
+        model = CleanedRoom
+        fields = ['occupied_rooms', 'missing_objects', 'need_towels', 'additional_comments']
