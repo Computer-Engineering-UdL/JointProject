efdbf0e190ea21cec462be99ced6e4c1dc100ce1
Sergi Fernandez Espona <s.nandes@hotmail.com>
2024-04-20 23:53:26 +0200

    feat: added cleanedRoom model

--- Cleaner/forms.py
+++ Cleaner/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from Cleaner.models import Stock, Cleaning_Material
+from Reception.models import Room
 
 
 class StockForm(forms.ModelForm):
@@ -9,3 +10,10 @@ class StockForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ['material']
+
+
+class CleanedRoomForm(forms.Form):
+    occupied_rooms = forms.ModelChoiceField(queryset=Room.objects.filter(is_taken=True,
+                                                                         roomreservation__check_out_active=False),
+                                            empty_label="Select a client")
+
