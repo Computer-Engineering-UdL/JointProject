1460d4063d6ba84e8ec6c183dd7521025a02b0f0
Aniol <aniol0012@gmail.com>
2024-04-24 15:40:51 +0200

    fix: update restaurant urls

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -3,5 +3,8 @@ from django.views.generic import TemplateView
 from Restaurant import views as v
 
 urlpatterns = [
-    path("", v.restaurant_home, name="cleaner_home"),
+    path("", v.restaurant_home, name="restaurant_home"),
+    path("new-reservation-1/", v.new_restaurant_reservation_1, name="new_restaurant_reservation_1"),
+    path("new-reservation-2/", v.new_restaurant_reservation_2, name="new_restaurant_reservation_2"),
+    path("reservations/", v.restaurant_reservations, name="restaurant_reservations"),
 ]
