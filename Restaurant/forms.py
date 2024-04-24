939fad4bf05be703e4ec86709ce2b5692d1865cf
Aniol <aniol0012@gmail.com>
2024-04-24 15:53:52 +0200

    feat: new restaurant reservation step 1

--- Restaurant/forms.py
+++ Restaurant/forms.py
@@ -1,1 +1,18 @@
 from django import forms
+from Restaurant.models import RestaurantReservation
+from datetime import date
+
+
+class NewRestaurantReservationForm(forms.ModelForm):
+    day = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), initial=date.today, label='Dia')
+    num_guests = forms.ChoiceField(choices=[(i, i) for i in range(1, 7)], label='Nombre de clients')
+
+    class Meta:
+        model = RestaurantReservation
+        fields = ['day', 'num_guests']
+
+    def clean_day(self):
+        day = self.cleaned_data.get('day')
+        if day < date.today():
+            raise forms.ValidationError("No es pot reservar per a un dia passat")
+        return day
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,8 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from User.decorators import worker_required
 from Restaurant.config import Config as c
+from Restaurant.forms import NewRestaurantReservationForm
+from Restaurant.models import RestaurantReservation
 
 
 @worker_required('restaurant')
 def restaurant_home(request):
     return render(request, c.get_restaurant_home_path(1))
+
+
+@worker_required('restaurant')
+def new_restaurant_reservation_1(request):
+    if request.method == 'POST':
+        form = NewRestaurantReservationForm(request.POST)
+        if form.is_valid():
+            request.session['reservation_data'] = request.POST
+            return redirect('new_restaurant_reservation_2')
+    else:
+        form = NewRestaurantReservationForm()
+
+    return render(request, c.get_restaurant_new_reservation_path(1), {'form': form})
+
+
+@worker_required('restaurant')
+def new_restaurant_reservation_2(request):
+    reservation_data = request.session.get('reservation_data')
+    if not reservation_data:
+        return redirect('new_restaurant_reservation_1')
+
+    if request.method == 'POST':
+        form = NewRestaurantReservationForm(reservation_data)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.client = request.user
+            reservation.save()
+            del request.session['reservation_data']
+            return redirect('restaurant_home')
+        else:
+            return render(request, c.get_restaurant_new_reservation_path(2), {'form': form})
+    else:
+        form = NewRestaurantReservationForm(initial=reservation_data)
+        return render(request, c.get_restaurant_new_reservation_path(2), {'form': form})
+
+
+@worker_required('restaurant')
+def restaurant_reservations(request):
+    reservations = RestaurantReservation.objects.filter(is_active=True)
+    return render(request, c.get_restaurant_check_reservations_path(1), {'reservations': reservations})
