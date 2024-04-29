54256da9c8510244d915ee53579e232e5244d127
Aniol0012 <aniol0012@gmail.com>
2024-04-29 20:04:49 +0200

    fix: add models to admin panel

--- Cleaner/admin.py
+++ Cleaner/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Cleaner.models import CleaningMaterial, Stock
+from Cleaner.models import CleaningMaterial, Stock, CleanedRoom
 
 admin.site.register(CleaningMaterial)
 admin.site.register(Stock)
+admin.site.register(CleanedRoom)
