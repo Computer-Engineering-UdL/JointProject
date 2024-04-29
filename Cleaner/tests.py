9df039ad147e1884013321e9150a5718e50a4ac4
Abdellah Lamrabat <udlunieps@gmail.com>
2024-04-29 23:16:18 +0200

    feat: status code test of cleaner are implemented

--- Cleaner/tests.py
+++ Cleaner/tests.py
@@ -4,7 +4,8 @@ from django.urls import reverse
 from django.utils import timezone
 from django import forms
 from Reception.models import RoomReservation, Client, Room, Worker
-from Reception.config import Config as c
+from Cleaner.models import Stock, CleaningMaterial, CleanedRoom
+from Cleaner.config import Config as c
 from Reception.forms import SearchReservationForm
 
 
@@ -37,13 +38,13 @@ class BaseTest(TestCase):
             room_type='Double'
         )
 
-        self.reservation = RoomReservation.objects.create(
-            client=self.client_user,
-            room=self.room,
-            entry=timezone.now().date(),
-            exit=(timezone.now() + timezone.timedelta(days=1)).date(),
-            pension_type='Sense pensió',
-            num_guests=2
+        self.cleaning_material_esponja = CleaningMaterial.objects.create(
+            material_name='Esponja',
+            image='esponja.png'
+        )
+        self.cleaning_material_spray = CleaningMaterial.objects.create(
+            material_name='Spray',
+            image='spray.png'
         )
 
         self.client.force_login(self.worker)
@@ -54,7 +55,7 @@ class TestStatusCode(BaseTest):
     def test_cleaner_home_status_code(self):
         url = reverse('cleaner_home')
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
     def test_cleaner_stock_status_code(self):
         url = reverse('cleaner_stock')
@@ -65,3 +66,16 @@ class TestStatusCode(BaseTest):
         url = reverse('cleaner_cleaned_rooms')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def test_cleaner_cleaner_cleaned_room_info_status_code(self):
+        url = reverse('cleaner_cleaned_room_info', args=[self.room.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+class TestCleanerForms(BaseTest):
+
+    def test_cleaner_stock_form(self):
+        url = reverse('cleaner_stock')
+        response = self.client.post(url, {'material_name': self.cleaning_material_esponja.material_name})
+        self.assertTemplateUsed(response, c.get_cleaner_stock_path(1))
+
