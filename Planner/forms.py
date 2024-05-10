75f78ccf35cfe216ec7eb1a80aa513550a564d99
Aniol0012 <aniol0012@gmail.com>
2024-05-10 20:24:35 +0200

    feat: improve validators

--- Planner/forms.py
+++ Planner/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 from Reception.models import Room
 from Reception.config import Config as c
 
@@ -13,3 +14,21 @@ class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ['room_num', 'room_type', 'is_clean', 'is_taken']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        room_num = cleaned_data.get('room_num')
+        room_type = cleaned_data.get('room_type')
+
+        try:
+            valid_range = c.get_room_number_range(room_type)
+            if not valid_range or not (valid_range[0] <= room_num <= valid_range[1]):
+                raise ValidationError(f'El número d\'habitació per {room_type} ha de '
+                                      f'ser entre {valid_range[0]} i {valid_range[1]}')
+        except TypeError:
+            raise ValidationError(f'El tipus d\'habitació seleccionat no és vàlid o no està ben configurat')
+
+        if Room.objects.filter(room_num=room_num).exists():
+            raise ValidationError('Aquest número d\'habitació ja està en ús')
+
+        return cleaned_data
