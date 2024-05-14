a6f0c7b55f35c2e68408a9ac5b58616173120dce
Aniol0012 <aniol0012@gmail.com>
2024-05-14 14:05:51 +0200

    fix: add id number unique

--- Reception/models.py
+++ Reception/models.py
@@ -6,7 +6,7 @@ from Reception.config import Config as c
 class HotelUser(AbstractUser):
     email = models.EmailField()
     phone_number = models.CharField(max_length=c.PHONE_NUMBER_LENGTH)
-    id_number = models.CharField(max_length=c.ID_NUMBER, blank=True)
+    id_number = models.CharField(max_length=c.ID_NUMBER, blank=True, unique=True)
 
     def __str__(self):
         return self.first_name
