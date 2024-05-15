7bcf00efd109a645fe25f043481ae129ef66c1f9
Aniol0012 <aniol0012@gmail.com>
2024-05-15 20:31:32 +0200

    fix: restaurant url

--- E2E/test_restaurant.py
+++ E2E/test_restaurant.py
@@ -7,7 +7,7 @@ def test_example(page: Page) -> None:
     page.get_by_label("Password").click()
     page.get_by_label("Password").fill("admin")
     page.get_by_role("button", name="Submit").click()
-    page.goto("http://localhost:8000/reservations/")
+    page.goto("http://localhost:8000/restaurant/")
     page.locator("#access-process-buttons").get_by_role("button", name="Nova reserva").click()
     page.locator("#id_day").fill("2024-07-18")
     page.locator("#id_num_guests").select_option("Dinar")
