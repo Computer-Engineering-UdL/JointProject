407706c85466d4fe62b4d5f42a030054b166af89
Aniol0012 <aniol0012@gmail.com>
2024-05-15 21:12:56 +0200

    fix: accountant urls

--- Accountant/urls.py
+++ Accountant/urls.py
@@ -4,4 +4,6 @@ from Accountant import views as v
 
 urlpatterns = [
     path('', v.accountant_home, name='accountant_home'),
+    path('cleaning-material', v.cleaning_material, name='cleaning_material'),
+    path('billing', v.billing_data, name='billing_data'),
 ]
--- Accountant/views.py
+++ Accountant/views.py
@@ -5,3 +5,12 @@ from User.decorators import worker_required
 @worker_required('accountant')
 def accountant_home(request):
     return render(request, 'worker/accountant/accountant_home.html')
+
+
+@worker_required('accountant')
+def cleaning_material(request):
+    return render(request, 'worker/accountant/cleaning_material.html')
+
+@worker_required('accountant')
+def billing_data(request):
+    return render(request, 'worker/accountant/billing_data.html')
